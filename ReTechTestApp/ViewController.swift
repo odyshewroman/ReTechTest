b02d05967a840968527f2a63a0049782fa04d380
ora <ora@tomsksoft.com>
2020-02-03 09:57:45 +0700

    init commit

---
+++ ReTechTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReTechTestApp
+//
+//  Created by ora on 03/02/2020.
+//  Copyright © 2020 Roman Odyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
